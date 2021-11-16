5a09382427ab24d73ab3c5a332262774be18f03c
Vanessa Santos <ricardosantosxav@gmail.com>
2021-11-16 19:30:34 -0300

    Add React with Typescript and setup colors constants

---
+++ src/constants/colors.ts
@@ -0,0 +1,14 @@
+export const COLORS = {
+  limeGreen: "hsl(163, 72%, 41%)",
+  brightRed: "hsl(356, 69%, 56%)",
+  white: "hsl(0, 0%, 100%)",
+
+  facebook: "hsl(208, 92%, 53%)",
+  twitter: "hsl(203, 89%, 53%)",
+  instagram: "linear-gradient(to right, hsl(37, 97%, 70%), hsl(329, 70%, 58%))",
+  youtube: "hsl(348, 97%, 39%)",
+
+  toggleLight: "hsl(230, 22%, 74%)",
+  toggleDark:
+    "linear-gradient(to right, hsl(210, 78%, 56%), hsl(146, 68%, 55%))",
+};
